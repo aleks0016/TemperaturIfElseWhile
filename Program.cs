6416826f5f1e2eaa23a948194b05b65e971d0eb7
Sikander06 <115735119+aleks0016@users.noreply.github.com>
2023-08-31 11:34:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperaturIfElseWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Skriv inn en temperatur: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int temperature))
+                {
+                    if (temperature > 0)
+                    {
+                        Console.WriteLine("Temperaturen er plussgrader.");
+                    }
+                    else if (temperature < 0)
+                    {
+                        Console.WriteLine("Temperaturen er minusgrader.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Temperaturen er null.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ugyldig, skriv inn et tall");
+                }
+            }
+                Console.ReadLine();
+        }
+    }
+}
